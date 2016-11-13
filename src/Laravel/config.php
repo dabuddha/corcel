a7a70c275bd8ba239dcedd78b7bfbe53a93cf02a
Junior Grossi <juniorgro@gmail.com>
2016-11-13 00:02:57 -0200

    Add a config file for Laravel framework

---
+++ src/Laravel/config.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+	/**
+	 * The WordPress database configuration
+	 */
+	'database' => [
+		'driver' => 'mysql',
+		'host' => env('WP_DB_HOST', 'localhost'),
+		'port' => env('WP_DB_PORT', '3306'),
+		'database' => env('WP_DB_DATABASE', 'wordpress'),
+		'username' => env('WP_DB_USERNAME', ''),
+		'password' => env('WP_DB_PASSWORD', ''),
+		'charset' => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix' => 'wp_',
+		'strict' => false,
+		'engine' => null,
+	],
+
+	/**
+	 * If you want to register custom shortcodes use this config array to map
+	 * each shortcode with a custom class
+	 */
+	'shortcodes' => [
+//		'example' => Path\To\Something::class,
+	],
+
+];
