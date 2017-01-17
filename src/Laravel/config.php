98d2cc4599f25694871ef1bb0493b610d011fc6d
Junior Grossi <juniorgro@gmail.com>
2017-01-17 00:26:34 -0200

    Set the connection name for Corcel classes

--- src/Laravel/config.php
+++ src/Laravel/config.php
@@ -3,21 +3,9 @@
 return [
 
 	/**
-	 * The WordPress database configuration
+	 * The connection name to be used by Corcel classes
 	 */
-	'database' => [
-		'driver' => 'mysql',
-		'host' => env('WP_DB_HOST', 'localhost'),
-		'port' => env('WP_DB_PORT', '3306'),
-		'database' => env('WP_DB_DATABASE', 'wordpress'),
-		'username' => env('WP_DB_USERNAME', 'root'),
-		'password' => env('WP_DB_PASSWORD', ''),
-		'charset' => 'utf8',
-		'collation' => 'utf8_unicode_ci',
-		'prefix' => 'wp_',
-		'strict' => false,
-		'engine' => null,
-	],
+	'connection' => 'corcel',
 
 	/**
 	 * If you want to register custom shortcodes use this config array to map
